b6f67cc9aed4aec236babc7d8f6b72988c075d07
cuzik <cuzikjack@gmail.com>
2019-04-29 13:47:16 -0300

    improve tests

--- test.rb
+++ test.rb
@@ -1,5 +1,1 @@
-require_relative 'tests/geometric_base_test'
-require_relative 'tests/square_test'
-require_relative 'tests/circle_test'
-require_relative 'tests/triangle_rectangle_test'
-require_relative 'tests/rectangle_test'
+Dir['./tests/**/*.rb'].each { |f| require f }
