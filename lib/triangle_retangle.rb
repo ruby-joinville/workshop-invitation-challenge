b6dc9ef6ac3b03eee6d21898f711905466d9ac77
cuzik <cuzikjack@gmail.com>
2019-04-29 13:02:35 -0300

    add triangle retangle

---
+++ lib/triangle_retangle.rb
@@ -0,0 +1,14 @@
+require_relative "geometric_base"
+
+class TriangleRectangle < GeometricBase
+  def initialize(base, height)
+  end
+
+  def area
+    0
+  end
+
+  def perimeter
+    0
+  end
+end
