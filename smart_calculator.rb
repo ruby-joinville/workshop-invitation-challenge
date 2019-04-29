6e305c3b021da2a384352350bd7b6ed07f4dd6fa
cuzik <cuzikjack@gmail.com>
2019-04-29 11:35:28 -0300

    add smart_calculator to based the math operations

---
+++ smart_calculator.rb
@@ -0,0 +1,19 @@
+module SmartCalculator
+  PI = 3.14
+
+  def sum(a, b)
+    a + b
+  end
+
+  def multiply(a, b)
+    a * b
+  end
+
+  def divide(a, b)
+    a / b
+  end
+
+  def squared(a)
+    a ** 2
+  end
+end
